831d72b1c32e8fbbfc89c277ae6753233478a546
Chiro <gabrielgx928@gmail.com>
2022-11-09 22:12:16 -0300

    atualização aula 3

--- pages/video.js
+++ pages/video.js
@@ -1,16 +1,24 @@
 import React from "react"
 import { ColorModeContext } from "../src/components/Menu/components/ColorMode";
+import styled from "styled-components";
 
+const StyledVideo = styled.div`
+    .button {
+        border-radius: 10px;
+
+    }
+
+`
 
 export default function Video() {
     const contexto = React.useContext(ColorModeContext);
     return (
-        <div>
+        <StyledVideo>
             Vídeo!
             {contexto.mode}
-            <button onClick={() => {contexto.toggleMode()}}>
+            <button className="button" onClick={() => {contexto.toggleMode()}}>
                 Trocar modo
             </button>
-        </div>
+        </StyledVideo>
     )
 }
